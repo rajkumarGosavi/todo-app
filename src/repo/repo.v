c1757dc143d4ce9bb0d6baec4d0fd1654a01bfd2
Rajkumar Gosavi <princegosavi12@gmail.com>
2023-05-06 15:30:19 +0530

    update store type -> struct

--- src/repo/repo.v
+++ src/repo/repo.v
@@ -2,28 +2,31 @@ module repo
 
 import todo
 
-pub type TodosStore = []todo.Todo
+pub struct TodosStore {
+pub mut:
+	todos []todo.Todo
+}
 
 // should return proper error
 pub fn (mut ts TodosStore) add_todo(td todo.Todo) {
 	mut t := td
-	t.id = ts.len + 1
-	ts << t
+	t.id = ts.todos.len + 1
+	ts.todos << t
 }
 
 // should return proper error
 pub fn (mut ts TodosStore) remove_todo(id int) {
-	ts[id - 1].is_deleted = !ts[id - 1].is_deleted
+	ts.todos[id - 1].is_deleted = !ts.todos[id - 1].is_deleted
 }
 
 // should return proper error
 pub fn (mut ts TodosStore) complete_todo(id int) {
-	ts[id - 1].is_completed = !ts[id - 1].is_completed
+	ts.todos[id - 1].is_completed = !ts.todos[id - 1].is_completed
 }
 
 pub fn (ts TodosStore) search_todo(txt string) []todo.Todo {
 	mut results := []todo.Todo{}
-	for td in ts {
+	for td in ts.todos {
 		if td.title.contains(txt) || td.description.contains(txt) {
 			results << td
 		}
@@ -34,17 +37,17 @@ pub fn (ts TodosStore) search_todo(txt string) []todo.Todo {
 
 // should return proper error
 pub fn (mut ts TodosStore) update_todo(td todo.Todo) {
-	ts[td.id - 1] = td
+	ts.todos[td.id - 1] = td
 }
 
 pub fn (ts TodosStore) get_all_todos() []todo.Todo {
-	return ts
+	return ts.todos
 }
 
 fn (ts TodosStore) get_todo(id int) ?todo.Todo {
-	if ts.len >= id {
+	if ts.todos.len >= id {
 		return none
 	}
 
-	return ts[id - 1]
+	return ts.todos[id - 1]
 }
--- src/repo/repo_test.v
+++ src/repo/repo_test.v
@@ -8,12 +8,14 @@ fn test_add_todo() {
 		title: 'title1'
 	}
 
-	mut ts := TodosStore([]todo.Todo{})
+	mut ts := TodosStore{
+		todos: []todo.Todo{}
+	}
 
 	ts.add_todo(td)
 
-	assert ts.len == 1
-	assert ts[0].title == 'title1'
+	assert ts.todos.len == 1
+	assert ts.todos[0].title == 'title1'
 
 	td2 := todo.Todo{
 		description: 'desc2'
@@ -22,7 +24,7 @@ fn test_add_todo() {
 
 	ts.add_todo(td2)
 
-	assert ts.len == 2
+	assert ts.todos.len == 2
 }
 
 fn test_remove_todo() {
@@ -31,13 +33,15 @@ fn test_remove_todo() {
 		title: 'title1'
 	}
 
-	mut ts := TodosStore([]todo.Todo{})
+	mut ts := TodosStore{
+		todos: []todo.Todo{}
+	}
 
 	ts.add_todo(td)
 
 	ts.remove_todo(1)
 
-	assert ts[0].is_deleted == true
+	assert ts.todos[0].is_deleted == true
 }
 
 fn test_complete_todo() {
@@ -46,17 +50,19 @@ fn test_complete_todo() {
 		title: 'title1'
 	}
 
-	mut ts := TodosStore([]todo.Todo{})
+	mut ts := TodosStore{
+		todos: []todo.Todo{}
+	}
 
 	ts.add_todo(td)
 
 	ts.complete_todo(1)
 
-	assert ts[0].is_completed == true
+	assert ts.todos[0].is_completed == true
 
 	ts.complete_todo(1)
 
-	assert ts[0].is_completed == false
+	assert ts.todos[0].is_completed == false
 }
 
 fn test_get_all_todos() {
@@ -65,13 +71,15 @@ fn test_get_all_todos() {
 		title: 'title1'
 	}
 
-	mut ts := TodosStore([]todo.Todo{})
+	mut ts := TodosStore{
+		todos: []todo.Todo{}
+	}
 	assert ts.get_all_todos().len == 0
 
 	ts.add_todo(td)
 
 	assert ts.get_all_todos().len == 1
-	assert ts[0].title == 'title1'
+	assert ts.todos[0].title == 'title1'
 }
 
 fn test_get_todo() {
@@ -80,8 +88,11 @@ fn test_get_todo() {
 		title: 'title1'
 	}
 
-	mut ts := TodosStore([]todo.Todo{})
-	assert ts.len == 0
+	mut ts := TodosStore{
+		todos: []todo.Todo{}
+	}
+
+	assert ts.todos.len == 0
 	assert ts.get_todo(5) == none
 
 	ts.add_todo(td)
@@ -97,13 +108,16 @@ fn test_update_todo() {
 		title: 'title1'
 	}
 
-	mut ts := TodosStore([]todo.Todo{})
+	mut ts := TodosStore{
+		todos: []todo.Todo{}
+	}
+
 	ts.add_todo(td)
 
-	mut td2 := ts[0]
+	mut td2 := ts.todos[0]
 	td2.title = 'updated title'
 
 	ts.update_todo(td2)
 
-	assert ts[0].title == td2.title
+	assert ts.todos[0].title == td2.title
 }
